1575ff0b0d0114c3cda8aad9d35a7e7caf7548db
dziwanski <anw93iw@gmail.com>
2020-11-02 19:53:02 +0100

    adding files for github pages

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import { CardList } from './components/card-list/card-list.component';
+import SearchBox from './components/search-box/search-box.component'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+	constructor() {
+		super();
+
+		this.state = {
+			monsters: [],
+			searchField: ''
+		}
+	}
+
+	componentDidMount() {
+		console.log('App.js componentDidMount()')
+		fetch('https://jsonplaceholder.typicode.com/users')
+			.then(response => response.json())
+			.then(users => this.setState({ monsters: users }))
+	}
+
+	componentDidUpdate() {
+		console.log('App.js componentDidUpdate()')
+	}
+
+	handleInput = (event) => {
+		this.setState({
+			searchField: event.target.value
+		}, () => console.log(`searchField został ustawiony: ${this.state.searchField}`));
+		console.log(`Wartosc w handleInput po lini wyzwalajacej setState: ${this.state.searchField}`)
+	}
+
+	render() {
+		console.log(`App.js render()`);
+
+		const { monsters, searchField } = this.state;
+		let searchedMonster = monsters.filter(monster => (
+			monster.name.toLowerCase().includes(searchField.toLowerCase())))
+		console.log(searchedMonster);
+		console.log('abcde'.includes(''));
+
+		return (
+			<div className="App">
+				<h1>Monsters Rolodex</h1>
+				<SearchBox placeholder='Search monster' handleChange={this.handleInput} />
+				<CardList monsters={searchedMonster} />
+			</div>
+		);
+	}
 }
 
 export default App;
